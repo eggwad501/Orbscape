e9787b4edc257b2d33458fba815679cde2119c8a
Pranav Srinivasan <srinivasan.pranav@utexas.edu>
2024-08-15 23:38:15 -0500

    core data - everything but end screen

--- Orbscape/Orbscape/ConfirmItemVC.swift
+++ Orbscape/Orbscape/ConfirmItemVC.swift
@@ -10,32 +10,6 @@ import CoreData
 
 class ConfirmItemVC: UIGameplayVC {
     
-    func retrieveItem(identifier: String) -> [NSManagedObject] {
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: identifier)
-        var fetchedResults: [NSManagedObject]? = nil
-        
-        do {
-            try fetchedResults = context.fetch(request) as? [NSManagedObject]
-        } catch {
-            print("Error occured while retrieving data")
-            abort()
-        }
-        return fetchedResults!
-    }
-    
-    func saveContext() {
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
     var delegate: UIGameplayVC!
     
     var itemImage: UIImage?
@@ -103,14 +77,18 @@ class ConfirmItemVC: UIGameplayVC {
         self.dismiss(animated: true, completion: nil)
         super.showNavigationBar()
         
+        localStore = PurchasableItems()
+        
         let vc = delegate as! ItemSelectChanger
         vc.removeBlurredBackgroundView()
                 
         if itemIndex != -1 {
             switch types {
+                
+            //skins
             case CustomizeTypes.skins?:
                 
-                let savedSkins = retrieveItem(identifier: "Skin")
+                let savedSkins = localStore.retrieveItem(identifier: "Skin")
                 
                 if skinsList[itemIndex].purchased {
                     currentSkin = skinsList[itemIndex]
@@ -120,19 +98,20 @@ class ConfirmItemVC: UIGameplayVC {
                     
                     //save updated skin to core data
                     savedSkins[itemIndex].setValue(true, forKey: "purchased")
-                    saveContext()
                     currentStarsCount -= itemCost
-                    retrieveItem(identifier: "Player")[0].setValue(currentStarsCount, forKey: "stars")
-                    saveContext()
+                    localStore.retrieveItem(identifier: "Player")[0].setValue(currentStarsCount, forKey: "stars")
+                    localStore.saveContext()
                 }
                 
                 for i in 2...8{
                     savedSkins[i - 1].setValue(itemIndex, forKey: "equippedIndex")
                 }
-                saveContext()
+                localStore.saveContext()
+               
                 
+            //sounds
             case CustomizeTypes.soundEffects?:
-                let savedSounds = retrieveItem(identifier: "Sound")
+                let savedSounds = localStore.retrieveItem(identifier: "Sound")
                 if soundsList[itemIndex].purchased {
                     currentSound = soundsList[itemIndex]
                 } else {
@@ -140,19 +119,20 @@ class ConfirmItemVC: UIGameplayVC {
                     soundsList[itemIndex].purchased = true
                     savedSounds[itemIndex].setValue(true, forKey: "purchased")
                     currentStarsCount -= itemCost
-                    retrieveItem(identifier: "Player")[0].setValue(currentStarsCount, forKey: "stars")
-                    saveContext()
+                    localStore.retrieveItem(identifier: "Player")[0].setValue(currentStarsCount, forKey: "stars")
+                    localStore.saveContext()
                 }
                 for i in 2...8{
                     savedSounds[i - 1].setValue(itemIndex, forKey: "equippedIndex")
                 }
-                saveContext()
+                localStore.saveContext()
                 
+            //themes
             case CustomizeTypes.themes?:
                 
                 //retrieve whwther it has been purchased from core data
                 
-                let savedThemes = retrieveItem(identifier: "Theme")
+                let savedThemes = localStore.retrieveItem(identifier: "Theme")
                 
                 // check value of bool 'purchased'
                 if themesList[itemIndex].purchased {
@@ -164,17 +144,16 @@ class ConfirmItemVC: UIGameplayVC {
                     
                     //save the value of puchased to core data
                     savedThemes[itemIndex].setValue(true, forKey: "purchased")
-                    saveContext()
                     currentStarsCount -= itemCost
-                    retrieveItem(identifier: "Player")[0].setValue(currentStarsCount, forKey: "stars")
-                    saveContext()
+                    localStore.retrieveItem(identifier: "Player")[0].setValue(currentStarsCount, forKey: "stars")
+                    localStore.saveContext()
                 }
                 
                 //save last equipped into core data
                 for i in 2...8{
                     savedThemes[i - 1].setValue(itemIndex, forKey: "equippedIndex")
                 }
-                saveContext()
+                localStore.saveContext()
                 vc.updateBackground(index: itemIndex)
                 
             default:
